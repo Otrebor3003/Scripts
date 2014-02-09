067e0c9c24190fe12f77e8225bc4e08d5cfe3692
otrebor3003 <roberto.casado@colectivo9.org>
2014-02-09 20:38:32 +0100

    Gestion de Directorios

--- Bash/normalizar-2.sh
+++ Bash/normalizar-2.sh
@@ -14,9 +14,92 @@ do
 	read respuesta
 	if [ $respuesta = si ];then
 		echo "modifico"
+		mv $F $nombre
 	else
 		echo "no modifico"
 	fi
-	 
-	
+	echo " Desea moverlo a algun directorio ?"
+	read respuestad
+	if [ $respuestad = si ];then
+
+			echo " listado "
+			echo " 1.	 ACCION"
+			echo " 2.	 BELICA"
+			echo " 3.	 CFICCION"
+			echo " 4.	 COMEDIA"
+			echo " 5.	 DIBUJOS"
+			echo " 6.	 DOCUMENTAL"
+			echo " 7.	 ESPAÑOLA"
+			echo " 8.	 LUCHA"
+			echo " 9.	 MALA"
+			echo " 10. 	 NUEVA"
+			echo " 11. 	 PINTURA"
+			echo " 12.	 POLICIACA"
+			echo " 13.	 ROMANTICA"
+			echo " 14.	 SUPERHEROES"
+			echo " 15.	 TERROR"
+			echo " 16.	 WESTERN"
+			echo " 17.	 ESPIAS"
+			echo " 18.	 DRAMA"
+
+		read tipo
+		case $tipo in
+			1 ) 
+			mv $nombre ACCION
+			;;
+			2 )
+			 mv $nombre BELICA
+			;;
+			3)
+			 mv $nombre CFICCION
+			;;
+			4)
+			 mv $nombre COMEDIA
+			;;
+			5)
+			 mv $nombre DIBUJOS
+			;;
+			6)
+			 mv $nombre DOCUMENTAL
+			;;
+			7)
+			 mv $nombre ESPAÑOLA
+			;;
+			8)
+			 mv $nombre LUCHA
+			;;
+			9)
+			 mv $nombre MALA
+			;;
+			10)
+			 mv $nombre NUEVA
+			;;
+			11)
+			 mv $nombre PINTURA
+			;;
+			12)
+			 mv $nombre POLICIACA
+			;;
+			13)
+			 mv $nombre ROMANTICA
+			;;
+			14)
+			 mv $nombre SUPERHEROES
+			;;
+			15)
+			 mv $nombre TERROR
+			;;
+			16)
+			 mv $nombre WESTERN
+			;;
+			17)
+			 mv $nombre ESPIAS
+			;;
+			18)
+			 mv $nombre DRAMA
+			;;
+		esac
+		else
+			echo "no modifico el directorio"
+		fi		 
 done
